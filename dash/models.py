c064f1fb831847862eb4069a5372b5c3403e1f90
ben <c@sa3.org>
2010-08-09 13:14:09 +0800

    change version to 0.0.5

--- dash/models.py
+++ dash/models.py
@@ -16,7 +16,7 @@ class Setting(db.Model):
     
     domain = db.StringProperty()
     timedelta = db.FloatProperty(default =8.0)
-    version = "0.0.4"
+    version = "0.0.5"
     
     @delmem("setting") 
     def put(self):
